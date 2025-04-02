95df5353b67e596682a2332bf32029c6996bf3b1
Yasser Vaz d'Almeida <yasilvalmeida@hotmail.com>
2025-04-02 21:23:58 +0100

    Add unit tests for contract

--- scripts/convert-key.ts
+++ scripts/convert-key.ts
@@ -15,6 +15,3 @@ const secretKey = Uint8Array.from(JSON.parse(keypairFile));
 // Convert to Base58 (for Phantom/Solflare)
 const privateKeyBase58 = bs58.encode(secretKey);
 console.log('Private Key (Base58):', privateKeyBase58);
-
-// Or keep it as a raw byte array (for Backpack)
-console.log('Private Key (Bytes):', Array.from(secretKey));
